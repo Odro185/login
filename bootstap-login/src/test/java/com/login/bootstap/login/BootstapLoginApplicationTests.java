57991d10dee538b329bba0d33c51d150fd88041b
Odro185 <68790804+Odro185@users.noreply.github.com>
2020-07-27 14:21:42 -0500

    Add files via upload

---
+++ bootstap-login/src/test/java/com/login/bootstap/login/BootstapLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.login.bootstap.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstapLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
